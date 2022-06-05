bcf57287a470b15aee8ca500f13292ac1759719d
ilybcmu <ibankole@andrew.cmu.edu>
2022-06-05 18:18:49 -0400

    the correct commit with updated shared dataloader

--- EncoderDecoderDataloaders.py
+++ EncoderDecoderDataloaders.py
@@ -32,19 +32,42 @@ import zipfile
 import nltk
 from nltk.corpus import wordnet
 from dataloading_utils import filter_negative_hundred, TransformerCompatDataset, get_num_examples, get_validation_acc, get_dataset_mapping
+from ArkDataset.load_ark import load_ark
+from TPANNDataset.load_tpann import load_tpann
+from TweeBankDataset.load_tweebank import load_tweebank
 
-
-
+ark_train, ark_val, ark_test = load_ark()
+tpann_train, tpann_val, tpann_test = load_tpann()
+tweebank_train, tweebank_val, tweebank_test = load_tweebank()
+ark_all = ark_train + ark_val + ark_test
+tweebank_all = tweebank_train + tweebank_val + tweebank_test
 # Needed Imports
 warnings.filterwarnings('ignore')
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("Device: ", device)
+def get_shared_examples(ark_all,twee_all):
+  """Takes as input all examples from ark and twee bank, then returns a list of shared examples of the format
+  [shared_x,ark_label,twee_label]"""
+  shared_examples = []
+  for ark in tqdm(ark_all):
+    for twee in tweebank_all:
+      if ark[0]==twee[0]:
+        shared_examples.append([])
+        shared_examples[-1].extend([ark[0],ark[1],twee[1]]) 
+  print("Number of shared Examples: ", len(shared_examples))
+  return shared_examples
+shared_examples = get_shared_examples(ark_all,tweebank_all)
 
-with open("ark_twee_shared.pkl",'rb') as f:
-  shared_examples = pickle.load(f)
 
-
+def get_dataloader(model_name:str, dataset, batch_size, shuffle=False):
+    tokenizer = AutoTokenizer.from_pretrained(model_name, add_prefix_space=True, use_fast=True, model_max_length=512)
+    if model_name == 'gpt2':
+        tokenizer.pad_token = tokenizer.eos_token
+    data_collator = DataCollatorForTokenClassification(tokenizer)
+    compat_dataset = TransformerCompatDataset(dataset, tokenizer)
+    dataloader = DataLoader(compat_dataset, shuffle=shuffle, batch_size=batch_size, collate_fn=data_collator)
+    return dataloader
 class ArkSharedDataset(torch.utils.data.Dataset):
     def __init__(self, shared_examples,test=False):
         self.shared_examples = shared_examples
@@ -93,11 +116,35 @@ twee_shared = TweeSharedDataset(shared_examples,test=False)
 ark_shared_dataloader = get_dataloader("gpt2",ark_shared,20,shuffle=False )
 twee_shared_dataloader = get_dataloader("gpt2",twee_shared,20,shuffle=False )
 
-print(len(ark_shared))
-print(len(twee_shared))
+# print(len(ark_shared))
+# print(len(twee_shared))
 
-for x,xx in zip(ark_shared_dataloader,twee_shared_dataloader):
-  batch = x
-  batch['twee_labels'] = xx['labels']
-  print(batch)
+
+class SharedDataLoader:
+    def __init__(self,shared_examples,batch_size):
+        self.ark_shared_data = ArkSharedDataset(shared_examples,test=False)
+        self.twee_shared_data = TweeSharedDataset(shared_examples,test=False)
+
+        self.ark_loader =get_dataloader("gpt2",self.ark_shared,batch_size,shuffle=False )
+        self.twee_loader =get_dataloader("gpt2",self.twee_shared,batch_size,shuffle=False )
+        assert len(self.ark_shared_data) == len(self.twee_shared_data)
+        self.length = len(self.ark_shared_data)
+    def __iter__(self):
+        for dataArk,dataTwee in zip(self.ark_loader,self.twee_loader):
+          batch = dataArk
+          batch['twee_labels'] = dataTwee['labels']
+          x = {}
+          for key in batch:
+            if key !="labels" and key!="twee_labels":
+              x[key] = batch[key]
+            elif key =="labels":
+              y = batch[key]
+            elif key == "twee_labels":
+              z = batch[key]
+          yield x,y,z
+SharedLoader = SharedDataLoader(shared_examples,batch_size=20)
+for x,y,z in SharedLoader:
+  print(x)
+  print(y)
+  print(z)
   break
