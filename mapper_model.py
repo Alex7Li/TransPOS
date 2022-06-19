2dd1d329e7fdbf5b1d6d4843e479137fa056cd17
Alex Li <7alex7li@gmail.com>
2022-06-19 14:18:48 -0700

    Add harden label option back in

--- mapper_model.py
+++ mapper_model.py
@@ -61,7 +61,7 @@ class MapperModel(torch.nn.Module):
 
         # Make the soft labels look similar to the hard labels so the model
         # is tricked into thinking they are the same or something
-        self.harden_label = False
+        self.harden_label = True
         self.to(device)
 
 
